895f926fc51429bdf9efc37abd994e9ae52f4692
Justin P <Jpartain89@gmail.com>
2018-07-25 03:37:58 -0500

    Fixed the `install.sh` script
    I was mistakenly forcing the system to use `pip3` on macOS rather than `pip2`, which is whats needed.

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,7 +12,13 @@ if [[ "$(uname)" == "Linux" ]]; then
 else
     # This is to test for whether the macOS has a system-
     # installed pip, or a homebrew-installed pip2
-    PIP_BIN="$(command -v pip3 || command -v pip2)"
+    if [[ -z "$(command -v pip2)" ]]; then
+        PIP_BIN="$(command -v pip2)"
+    elif [[ -z "$(command -v pip3)" ]]; then
+        echo "Sorry, right now Sphinx-Doc uses python 2.7. "
+        echo "Please install python 2.7"
+        exit 1
+    fi
 fi
 
 sudo -H "$PIP_BIN" install --upgrade -r "${DIR}/requirements.txt"
