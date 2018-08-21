a60179da887cfcdc1b7c2c91730b1dd04fddb47d
Justin P <Jpartain89@gmail.com>
2018-08-21 16:53:27 -0500

    Removed underscore from bash variable

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,17 +8,17 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ "$(uname)" == "Linux" ]]; then
     . /etc/*release
-    PIP_BIN="$(command -v pip2)"
+    PIPBIN="$(command -v pip2)"
 else
     # This is to test for whether the macOS has a system-
     # installed pip, or a homebrew-installed pip2
-    if [[ -z "$(command -v pip2)" ]]; then
-        PIP_BIN="$(command -v pip2)"
-    elif [[ -z "$(command -v pip3)" ]]; then
+    if [[ ! -z "$(command -v pip2)" ]]; then
+        PIPBIN="$(command -v pip2)"
+    else
         echo "Sorry, right now Sphinx-Doc uses python 2.7. "
         echo "Please install python 2.7"
         exit 1
     fi
 fi
 
-sudo -H "$PIP_BIN" install --upgrade -r "${DIR}/requirements.txt"
+sudo -H "${PIPBIN}" install --upgrade -r "${DIR}/requirements.txt"
