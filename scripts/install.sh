bfcb5ac0cef69bf885a680cad53eae04c10b9304
Justin P <jpartain89@gmail.com>
2019-05-22 14:38:28 -0500

    Update for 05-22-2019 14-38-28

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,16 +8,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [[ "$(uname)" == "Linux" ]]; then
     . /etc/*release
-    PIPBIN="$(command -v pip2)"
+    PIPBIN="$(command -v pip3)"
 else
-    # This is to test for whether the macOS has a system-
-    # installed pip, or a homebrew-installed pip2
-    if [[ ! -z "$(command -v pip2)" ]]; then
-        PIPBIN="$(command -v pip2)"
-    else
-        echo "Sorry, right now Sphinx-Doc uses python 2.7. "
-        echo "Please install python 2.7"
-        exit 1
+    if [[ ! -z "$(command -v pip3)" ]]; then
+        PIPBIN="$(command -v pip3)"
     fi
 fi
 
