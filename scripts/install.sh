9645c72108742e9d8dfcce7df2c1c69c124b9025
Justin P <jpartain89@gmail.com>
2023-08-03 13:19:26 -0500

    Updated install.sh and updated the download link for Download-Xcode-with-wget

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,17 +7,19 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if [[ "$(command -v virtualenv)" ]]; then
-    if [[ ! -e "${DIR}/../venv" ]]; then
-        virtualenv "${DIR}/../venv"
-    fi
-else
-    echo "Looks like 'virtualenv' is missing from your computer..."
-    echo "Please, install that program, first!"
-    echo "Thanks, and have a grand day!"
-    exit
-fi
+#if [[ "$(command -v virtualenv)" ]]; then
+#    if [[ ! -e "${DIR}/../venv" ]]; then
+#        virtualenv "${DIR}/../venv"
+#    fi
+#else
+#    echo "Looks like 'virtualenv' is missing from your computer..."
+#    echo "Please, install that program, first!"
+#    echo "Thanks, and have a grand day!"
+#    exit
+#fi
 
-source "${DIR}/../venv/bin/activate"
+#source "${DIR}/../venv/bin/activate"
+#
+#"${DIR}/../venv/bin/pip3" install --upgrade -r "${DIR}/../requirements.txt"
 
-"${DIR}/../venv/bin/pip3" install --upgrade -r "${DIR}/../requirements.txt"
+pipenv install
