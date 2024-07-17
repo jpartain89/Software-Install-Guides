da692078005c4f6e8e61afa8e13591f6737f6a3c
Justin P <Jpartain89@gmail.com>
2024-07-16 22:55:54 -0500

    Changing install.sh to older command for testing

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,22 +7,22 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-#if [[ "$(command -v virtualenv)" ]]; then
-#    if [[ ! -e "${DIR}/../venv" ]]; then
-#        virtualenv "${DIR}/../venv"
-#    fi
-#else
-#    echo "Looks like 'virtualenv' is missing from your computer..."
-#    echo "Please, install that program, first!"
-#    echo "Thanks, and have a grand day!"
-#    exit
-#fi
+if [[ "$(command -v virtualenv)" ]]; then
+    if [[ ! -e "${DIR}/../venv" ]]; then
+        virtualenv "${DIR}/../venv"
+    fi
+else
+    echo "Looks like 'virtualenv' is missing from your computer..."
+    echo "Please, install that program, first!"
+    echo "Thanks, and have a grand day!"
+    exit
+fi
 
-#source "${DIR}/../venv/bin/activate"
+source "${DIR}/../venv/bin/activate"
+
+"${DIR}/../venv/bin/pip3" install --upgrade -r "${DIR}/../requirements.txt"
+
+#pip3 install --upgrade pip
+#pip3 install -U pipenv
 #
-#"${DIR}/../venv/bin/pip3" install --upgrade -r "${DIR}/../requirements.txt"
-
-pip3 install --upgrade pip
-pip3 install -U pipenv
-
-$(command -v pipenv) install
+#$(command -v pipenv) install
